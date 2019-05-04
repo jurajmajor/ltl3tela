2f1ed2ceb8b5a8b0920bee235b3d68a494802712
xblahoud <fandikb@gmail.com>
2019-05-05 01:33:50 +0200

    generating tables

--- Experiments/evaluation_utils.py
+++ Experiments/evaluation_utils.py
@@ -33,3 +33,34 @@ def split_cols(res,symbol='/',axis=1,names=None,repl_empty=True):
     elif axis == 0:
         tmp.index = new
     return tmp #tmp.sort_index(axis=axis,level=[0],sort_remaining=False)
+
+def high_min(data):
+    return ['\high ${:0.0f}$'.format(m) if 
+            m == data.min() else m for m in data]
+
+def high_max(data):
+    return ['\high ${:0.0f}$'.format(m) if 
+            m == data.max() else m for m in data]
+
+def fix_tool(t):
+    t = t.replace('-D',' -D')
+    t = t.replace('-G',' -G')
+    t = t.replace('delag','Delag')
+    t = t.replace('rabinizer4','Rabinizer 4\\tgdra')
+    t = t.replace('ltl3ba','ltl3ba -H2\\tgba')
+    if t == 'ltl2tgba':
+        t = 'ltl2tgba\\tgba'
+    if t.startswith('ltl'):
+        t = f'\\spottool{{{t}}}'
+    return t
+
+def fix_type(t,vertical=False,color=None):
+    t = t.replace('deterministic','det.')
+    if color:
+        source, t = t.split('_')
+        t = '\\parbox[c]{{12.9ex}}{{\centering {}}}'.format(t)
+        if source == color:
+            t = high_frag(t)
+    if vertical:
+        t = '\rotatebox[origin=c]{90}{\\footnotesize ' + t + '}'
+    return t
